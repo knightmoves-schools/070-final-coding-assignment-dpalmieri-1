087a176ff168b45612f9e3d2e3e9d44771881d97
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-05-06 19:40:59 +0000

    Commit

--- Game.cs
+++ Game.cs
@@ -4,14 +4,14 @@ using System;
 
 class Game : knightmoves.Cheatable
 {
-    private readonly string phrase;
+    private readonly string _phrase;
     
     private const string MAGIC_WORD = "CHEAT";
     private const int DIVISIBLE_BY_VALUE = 2;
 
     public Game(string Phrase)
     {
-        this.phrase = Phrase;
+        _phrase = Phrase;
     }
 
     /// <summary>
@@ -19,7 +19,7 @@ class Game : knightmoves.Cheatable
     /// </summary>
     public string DisplayBlanks()
     {
-        return new string(phrase.Select(c => char.IsLetter(c) ? '_' : c).ToArray());
+        return new string(_phrase.Select(c => c == ' ' ? ' ' : '_').ToArray());
     }
 
     /// <summary>
@@ -35,11 +35,11 @@ class Game : knightmoves.Cheatable
         if(ApplyEasterEggCheat(guessedLettersStr))
         {
             Console.WriteLine();
-            return string.Join(" ", phrase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(word => string.Concat(word.Select(c => char.IsLetter(c) ? c : c))));
+            return string.Join(" ", _phrase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(word => string.Concat(word.Select(c => char.IsLetter(c) ? c : c))));
         }
 
         // Build normal reveal
-        string result = new string(phrase.Select(c =>
+        string result = new string(_phrase.Select(c =>
         {
             if (char.IsLetter(c) && guessedLetters.Any(g => char.ToLower(g) == char.ToLower(c)))
                 return c;
@@ -55,7 +55,7 @@ class Game : knightmoves.Cheatable
         }
 
         // Apply time cheat if active
-        if (timeCheatActive == true || timeCheatActive == false)
+        if (timeCheatActive == true)
         {
             return ApplyTimeCheatReveal(result);
         }
@@ -78,13 +78,13 @@ class Game : knightmoves.Cheatable
 		Console.WriteLine();
         Console.WriteLine("Time cheat active! Revealing every other word...");
         Console.WriteLine();
-		
+
         return string.Join(' ', words);
     }
 
     private string GetOriginalWordAtPosition(int wordIndex)
     {
-        string[] originalWords = phrase.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] originalWords = _phrase.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         return wordIndex < originalWords.Length ? originalWords[wordIndex] : "";																						;
 	}
 	
@@ -94,12 +94,22 @@ class Game : knightmoves.Cheatable
 
     public bool ApplyTimeCheat(DateTime now, int divisibleByValue)
     {
-        if (divisibleByValue <= 0)
+        /*if (divisibleByValue <= 0)
+            return false;*/
+            Console.WriteLine(DateTime.Now);
+        if(divisibleByValue <= 0)
+        {
+            return now.Second % divisibleByValue == 0;
+        } else if (divisibleByValue > 0)
+        {
             return false;
-
-        return now.Second % divisibleByValue == 0;
+        } else
+        {
+            return false;
+        }
+        
     }
-
+    //change if return to if else
     public bool ApplyEasterEggCheat(string magicWord)
     {
         return magicWord.ToUpperInvariant().Contains(MAGIC_WORD);
@@ -115,86 +125,18 @@ class Game : knightmoves.Cheatable
 		
 		HashSet<char> Uniqueness = new HashSet<char>();
 		
-		foreach(char c in guessedLetters){
+		/*foreach(char c in guessedLetters){
 			if(!Uniqueness.Add(c)){
 				return false;
 			}
-		}
-        return guessedLetters.Length is <= 100 and >= 1 &&
+		}*/
+        for (int i = 0; i < guessedLetters.Length; i++)
+        {
+            char c = guessedLetters[i];
+            if (!Uniqueness.Add(c))
+                return false;
+        }
+        return guessedLetters.Length is <= 10 and >= 10 &&
                guessedLetters.All(char.IsLetter);
     }
 }
-
-
-
-/*
-class Game{
-    // Make your changes in this file
-    private string phrase;
-    private const string MAGIC_WORD = "CHEAT";
-    private const int DIVISIBLE_BY_VALUE = 2;
-
-    public Game(string phrase)
-    {
-        this.phrase = phrase;
-    }
-
-    public string DisplayBlanks()
-    {
-        return new string(phrase.Select(c => char.IsLetter(c) ? '_' : c).ToArray());
-    }
-
-    public string Play(char[] guessedLetters){
-        string guessedLettersStr = new string(guessedLetters);
-        
-        // Check for Easter Egg cheat (magic word)
-        if (ApplyEasterEggCheat(guessedLettersStr))
-        {
-            return string.Join(" ", phrase.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries).Select(word => string.Concat(word.Select(c => char.IsLetter(c) ? c : c))));
-        }
-        
-        // Check for time cheat
-        bool timeCheatActive = ApplyTimeCheat();
-        
-        string result = new string(phrase.Select(c => char.IsLetter(c) && guessedLetters.Contains(char.ToLower(c)) ? c : (char.IsWhiteSpace(c) ? c : '_')).ToArray());
-        
-        // Check if all letters have been guessed
-        if (!result.Contains('_'))
-        {
-            Console.Clear();
-            return "Congratulations";
-        }
-        
-        if (timeCheatActive)
-        {
-            // Reveal every other word starting at the second word
-            string[] words = result.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
-            for (int i = 1; i < words.Length; i += 2)
-            {
-                words[i] = new string(words[i].Select(c => char.IsLetter(phrase.FirstOrDefault(pc => char.ToLower(pc) == char.ToLower(c))) ? phrase.FirstOrDefault(pc => char.ToLower(pc) == char.ToLower(c)) : c).ToArray());
-            }
-            return string.Join(" ", words);
-        }
-        
-        return string.Join(" ", result.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries).Select(word => string.Concat(word.Select(c => c == '_' ? '_' : c))));
-    }
-    
-    public bool ApplyTimeCheat()
-    {
-        int currentSecond = System.DateTime.Now.Second;
-        return currentSecond % DIVISIBLE_BY_VALUE == 0;
-    }
-    
-    public bool ApplyEasterEggCheat(string guessedLetters)
-    {
-        return guessedLetters.ToUpper().Contains(MAGIC_WORD);
-    }
-        
-    public bool IsValid(string guessedLetters){
-        if(guessedLetters.Length > 10 || guessedLetters.Length < 10 || !guessedLetters.All(char.IsLetter)){
-            return false;
-        }
-        return true;
-    }
-}
-*/
